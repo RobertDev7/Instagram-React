d9eb1cd22bc865b285154ffaa8e124cb7d5e58f6
Robert Ramos <133607849+RobertDev7@users.noreply.github.com>
2023-09-04 22:21:10 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Home from './pages/Home/index';
+
+function App() {
+  return (
+    <div className="main">
+      <Home />
+    </div>
+  )
+}
+
+export default App;
